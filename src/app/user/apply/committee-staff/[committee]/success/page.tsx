89ccebc4abacff91b250aea037a0a76af704badb
Christian Buenagua <christianbhernan.buenagua.cics@ust.edu.ph>
2025-09-14 16:26:46 +0800

    removed eb and committee array. used the data provided instead

--- src/app/user/apply/committee-staff/[committee]/success/page.tsx
+++ src/app/user/apply/committee-staff/[committee]/success/page.tsx
@@ -5,6 +5,7 @@ import { useRouter, useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { committeeRoles } from "@/data/committeeRoles";
 
 export default function SuccessPage() {
   const router = useRouter();
@@ -19,69 +20,6 @@ export default function SuccessPage() {
       setScheduledTime(time);
     }
   }, []);
-
-  const committeeRoles = [
-    {
-      id: "academics",
-      title: "Academics",
-      description:
-        "This committee is dedicated to enhancing the academic environment within the CSS organization. It provides reviewers and organizes tutorials to support CSS students. Additionally, the committee organizes academic-related events, such as quiz bees and programming contests, fostering a vibrant intellectual community.",
-    },
-    {
-      id: "community",
-      title: "Community Development",
-      description:
-        "This committee works towards improving and sustaining the well-being of the local community. They devise and implement community-based projects and events that foster social interaction, civic engagement, and community empowerment. It may focus on areas such as housing, employment, health services, or environmental initiatives.",
-    },
-    {
-      id: "creatives",
-      title: "Creatives & Technical",
-      description:
-        "This committee oversees the design and production of all creative outputs of the organization, including digital graphics, promotional materials, and event decoration. The technical side of the committee ensures that all technical needs for events and operations, like sound and lighting systems, are catered for.",
-    },
-    {
-      id: "documentation",
-      title: "Documentation",
-      description:
-        "This committee is responsible for photojournalism, documenting all the activities and events of the organization. Their work ensures that the organization's achievements and memorable moments are captured and preserved for posterity.",
-    },
-    {
-      id: "external",
-      title: "External Affairs",
-      description:
-        "This committee manages relationships and communications with entities outside the organization. This includes liaising with other organizations, government bodies, sponsors, and the media. It also handles public relations, partnership development, and conflict resolution.",
-    },
-    {
-      id: "finance",
-      title: "Finance",
-      description:
-        "This committee oversees the organization's budgeting, expenditure, and revenue generation. It also provides financial advice to the organization, ensures fiscal responsibility, and conducts regular audits for transparency and accountability.",
-    },
-    {
-      id: "logistics",
-      title: "Logistics",
-      description:
-        "This committee manages and maintains all properties owned by the organization. It keeps a thorough record of all expenses related to CSS activities and properties, ensuring transparency and accountability in the organization's financial operations.",
-    },
-    {
-      id: "publicity",
-      title: "Publicity",
-      description:
-        "This committee manages all promotional activities for the organization. It is responsible for creating and implementing marketing strategies, managing social media platforms, and publicizing events and activities to target audiences.",
-    },
-    {
-      id: "sports",
-      title: "Sports & Talent",
-      description:
-        "This committee organizes and oversees all sports-related and talent activities within the organization. It may coordinate sporting events, talent shows, or workshops and ensure the organization's members have opportunities to develop and showcase their talents.",
-    },
-    {
-      id: "technology",
-      title: "Technology Development",
-      description:
-        "This committee is responsible for spearheading all technology-related projects and events within the organization. Key tasks include creating and maintaining the CSS website, implementing new technologies to streamline organizational operations, and organizing tech-focused workshops or seminars to enhance the digital skills of the members.",
-    },
-  ];
 
   const selectedCommittee = committeeRoles.find(
     (role) => role.id === committeeId
