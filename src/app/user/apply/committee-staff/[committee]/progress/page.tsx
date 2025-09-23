9f7302a6d2df488f53e5859d07b073d0e7aad464
Shrxmy <shroomyroomyk@gmail.com>
2025-09-23 15:48:37 +0800

    remove back to dashboard button

--- src/app/user/apply/committee-staff/[committee]/progress/page.tsx
+++ src/app/user/apply/committee-staff/[committee]/progress/page.tsx
@@ -478,13 +478,6 @@ function CommitteeProgressPageContent() {
           )}
 
           <div className="flex flex-col sm:flex-row justify-center gap-4">
-            <button
-                    onClick={() => router.push("/user")}
-                    className="bg-[#E7E3E3] text-gray-700 px-15 py-3 rounded-lg font-inter font-semibold text-sm hover:bg-[#CDCCCC] transition-all duration-150 active:scale-95"
-                >
-                    Back to Dashboard
-                </button>
-
             {!application.interviewSlotDay && (
               <button
                 onClick={() =>
